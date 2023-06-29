8a29f84284227000ed47607991fe285a240a78b1
CameronTheDeveloper <Cameronposey@outlook.com>
2023-06-28 22:56:04 -0700

    Add javascript validations

--- form.js
+++ form.js
@@ -1,11 +1,58 @@
 const form1 = document.querySelector('#form1');
 const emailInput = document.querySelector('#email');
+const countryInput = document.querySelector('#country');
+const zipInput = document.querySelector('#zip');
+const passwordInput = document.querySelector('#pswd');
+const passError = document.querySelector('#pswd + .pswd-error');
 
 const checkValid = (inputElement) => {
     if (inputElement.checkValidity()) {
-        console.log('Valid true');
-    } else if (emailInput.validity.typeMismatch) {
-        emailInput.setCustomValidity('Wrong format, please type email');
+        inputElement.setCustomValidity('');
+    } else if (inputElement.validity.typeMismatch) {
+        inputElement.setCustomValidity('Wrong format, please type email');
+    }
+};
+
+const checkCountryPattern = (inputElement) => {
+    if (inputElement.validity.patternMismatch) {
+        inputElement.setCustomValidity('Invalid country format (Only letters)');
+    } else {
+        inputElement.setCustomValidity('');
+    }
+};
+
+const checkZipValidity = (zipInput) => {
+    if (zipInput.validity.patternMismatch) {
+        inputElement.setCustomValidity('Invalid zip code');
+    } else {
+        inputElement.setCustomValidity('');
+    }
+};
+console.log(passError);
+
+passwordInput.addEventListener('input', () => {
+    if (!passwordInput.checkValidity()) {
+        if (passwordInput.validity.tooShort) {
+            passError.textContent = 'Too Short';
+        } else if (passwordInput.validity.tooLong) {
+            passError.textContent = 'Too Long';
+        }
+
+    } else {
+        passwordInput.setCustomValidity('');
+    }
+});
+
+
+const checkPasswordValidation = (passwordInput) => {
+    if (!passwordInput.checkValidity()) {
+        if (passwordInput.validity.tooShort) {
+            passError.textContent = 'Too Short';
+        } else if (passwordInput.validity.tooLong) {
+            passError.textContent = 'Too Long';
+        }
+    } else {
+        passwordInput.setCustomValidity('');
     }
 };
 
@@ -16,6 +63,8 @@ emailInput.addEventListener('input', () => {
 form1.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log('Submitted');
-    // checkValid(emailInput);
+    checkValid(emailInput);
+    checkCountryPattern(countryInput);
+    checkPasswordValidation(passwordInput);
 })
 
